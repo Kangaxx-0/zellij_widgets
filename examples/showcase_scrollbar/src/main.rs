4bf30e9c57552deb5340a7d68332da8d00999dc6
Frank <damn.frank2z@gmail.com>
2024-03-05 09:51:57 -0800

    Add horizontal scrllbar as well

--- examples/showcase_scrollbar/src/main.rs
+++ examples/showcase_scrollbar/src/main.rs
@@ -6,8 +6,16 @@ use zellij_widgets::prelude::*;
 struct State {
     is_loading: bool,
     pressed_key: char,
-    scroll_state: ScrollbarState,
+    scroll_state: ScrollbarStateInternal,
     text: Vec<String>,
+}
+
+#[derive(Default, Clone)]
+struct ScrollbarStateInternal {
+    pub vertical_scroll_state: ScrollbarState,
+    pub horizontal_scroll_state: ScrollbarState,
+    pub vertical_scroll: usize,
+    pub horizontal_scroll: usize,
 }
 
 register_plugin!(State);
@@ -25,7 +33,12 @@ impl ZellijPlugin for State {
         ]);
         self.is_loading = true;
         self.text = vec![r##"Hello, everyone! This is the LONGEST TEXT EVER! I was inspired by the various other "longest texts ever" on the internet, and I wanted to make my own. So here it is! This is going to be a WORLD RECORD! This is actually my third attempt at doing this. The first time, I didn't save it. The second time, the Neocities editor crashed. Now I'm writing this in Notepad, then copying it into the Neocities editor instead of typing it directly in the Neocities editor to avoid crashing. It sucks that my past two attempts are gone now. Those actually got pretty long. Not the longest, but still pretty long. I hope this one won't get lost somehow. Anyways, let's talk about WAFFLES! I like waffles. Waffles are cool. Waffles is a funny word. There's a Teen Titans Go episode called "Waffles" where the word "Waffles" is said a hundred-something times. It's pretty annoying. There's also a Teen Titans Go episode about Pig Latin. Don't know what Pig Latin is? It's a language where you take all the consonants before the first vowel, move them to the end, and add '-ay' to the end. If the word begins with a vowel, you just add '-way' to the end. For example, "Waffles" becomes "Afflesway". I've been speaking Pig Latin fluently since the fourth grade, so it surprised me when I saw the episode for the first time. I speak Pig Latin with my sister sometimes. It's pretty fun. I like speaking it in public so that everyone around us gets confused. That's never actually happened before, but if it ever does, 'twill be pretty funny. By the way, "'twill" is a word I invented recently, and it's a contraction of "it will". I really hope it gains popularity in the near future, because "'twill" is WAY more fun than saying "it'll". "It'll" is too boring. Nobody likes boring. This is nowhere near being the longest text ever, but eventually it will be! I might still be writing this a decade later, who knows?"##.to_string(), "line-1".to_string(),"line-2".to_string(),"line-3".to_string(),"line-4".to_string(),"line-5".to_string(),"line-6".to_string(),"line-7".to_string(),"line-8".to_string(),"line-0".to_string(),"line-10".to_string(),"line-11".to_string(),"line-12".to_string(),"line-13".to_string(),"line-14".to_string(),"line-15".to_string(),"line-16".to_string(),"line-17".to_string(),"line-18".to_string(),"line-19".to_string(),"line-20".to_string(),"line-21".to_string(),"line-22".to_string(),"line-23".to_string(),"line-24".to_string(),"line-25".to_string(),"line-26".to_string(),"line-27".to_string(),"line-28".to_string(),"line-29".to_string(),"line-30".to_string(),"line-31".to_string(),"line-32".to_string(),];
-        self.scroll_state = ScrollbarState::new(3);
+        self.scroll_state = ScrollbarStateInternal {
+            vertical_scroll_state: ScrollbarState::new(self.text.len()),
+            horizontal_scroll_state: ScrollbarState::new(self.text[0].len()),
+            vertical_scroll: 0,
+            horizontal_scroll: 0,
+        };
     }
 
     fn update(&mut self, event: Event) -> bool {
@@ -56,7 +69,7 @@ impl ZellijPlugin for State {
     }
 }
 
-fn ui(frame: &mut Frame, text: Vec<Line>, state: &ScrollbarState) {
+fn ui(frame: &mut Frame, text: Vec<Line>, state: &ScrollbarStateInternal) {
     let layouts = Layout::default()
         .direction(Orientation::Vertical)
         .constraints(
@@ -95,7 +108,12 @@ fn render_title(frame: &mut Frame, area: Geometry) {
     );
 }
 
-fn render_scrollbar(frame: &mut Frame, area: Geometry, text: Vec<Line>, state: &ScrollbarState) {
+fn render_scrollbar(
+    frame: &mut Frame,
+    area: Geometry,
+    text: Vec<Line>,
+    state: &ScrollbarStateInternal,
+) {
     frame.render_widget(
         Paragraph::new(text.clone())
             .block(Block::default().borders(Borders::ALL).title("paragraph"))
@@ -105,10 +123,27 @@ fn render_scrollbar(frame: &mut Frame, area: Geometry, text: Vec<Line>, state: &
     frame.render_state_widget(
         Scrollbar::default()
             .orientation(ScrollbarOrientation::VerticalRight)
-            .begin_symbol(Some("#"))
-            .end_symbol(Some("#")),
-        area,
-        state,
+            .symbols(style::symbols::scrollbar::VERTICAL)
+            .begin_symbol(None)
+            .track_symbol(None)
+            .end_symbol(None),
+        area.inner(&Margin {
+            horizontal: 0,
+            vertical: 1,
+        }),
+        &state.vertical_scroll_state,
+    );
+    frame.render_state_widget(
+        Scrollbar::default()
+            .orientation(ScrollbarOrientation::HorizontalBottom)
+            .begin_symbol(None)
+            .track_symbol(None)
+            .end_symbol(None),
+        area.inner(&Margin {
+            horizontal: 1,
+            vertical: 0,
+        }),
+        &state.horizontal_scroll_state,
     );
 }
 
