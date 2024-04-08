387be162e4f5742d62d824ee82e4c5beb940157f
Frank <damn.frank2z@gmail.com>
2024-04-08 16:03:47 -0700

    Another fix

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,13 @@
 //! This crate combines the [ratatui](https://docs.rs/ratatui/latest/ratatui/index.html) and [crossterm](https://docs.rs/crossterm/latest/crossterm/index.html)
 //! to provide a comprehensive set of widgets and utilities for building zellij plugin applications.
 //!
-//! [EXAMPLES]: https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples
-//!  - Block : https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_block
-//!  - List : https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_list
-//!  - Tab : https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_tabs
-//!  - Scrollbar : https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_scrollbar
-//!  - Popup : https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_popup
-//!  - Custom component : https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_custom_button
+//! [EXAMPLES]: <https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples>
+//!  - Block : <https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_block>
+//!  - List : <https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_list>
+//!  - Tab : <https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_tabs>
+//!  - Scrollbar : <https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_scrollbar>
+//!  - Popup : <https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_popup>
+//!  - Custom component : <https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/showcase_custom_button>
 //!
 //! For an end-to-end demonstration, check out the [session_manager](https://github.com/Kangaxx-0/zellij_widgets/tree/main/examples/e2e/session_manager) example,
 //! which provides a complete rewrite of an existing zellij plugin using `zellij_widgets`.
